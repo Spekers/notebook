0ea5c6850ebbe3ff93ff15a6156375ac9c7b55f2
Sipke Fokkema <sipke.fokkema@epfl.ch>
2026-05-02 17:12:03 +0200

    added domain name

--- bin/build/vars.sh
+++ bin/build/vars.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 export BLOG_NAME="Sip’s Notebook"
-export BLOG_URL="https://notebook.sip.com"
+export BLOG_URL="https://sips-log.com"
 export GIT_WEB_URL="https://github.com/Spekers/notebook/commit/"
